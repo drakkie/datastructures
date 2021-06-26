6106aa9fbd3cd56350c1792e8effb71fc6c31dcf
Stanley F <drakkie@gmail.com>
2021-06-25 23:50:40 -0700

    implement basics of arraylist

--- data structures/arraylist.cs
+++ data structures/arraylist.cs
@@ -3,16 +3,37 @@ namespace datastructures
     public class ArrayList<T>
     {
         private T[] _array;
+        private int _added;
 
         public ArrayList()
         {
             _array = new T[1];
+            _added = 0;
         }
 
         public void Add(T thing)
         {
+            _array[_added] = thing;
 
-            //double up
+            if (_added >= _array.Length)
+            {
+                DoubleUp();
+            }
+            
+        }
+
+        private void DoubleUp()
+        {
+            var len = _array.Length * 2;
+            var newArray = new T[len];
+
+            var i = 0;
+            foreach (var item in _array)
+            {
+                newArray[i++] = item;
+            }
+
+            _array = newArray;
         }
     }
 }
